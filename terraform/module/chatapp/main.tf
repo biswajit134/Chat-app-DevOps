5f6524c2d374f2a84471b6004ab92722321fb880
biswajit134 <biswajithazra133@gmail.com>
2026-05-02 20:24:36 +0530

    Add helm

--- terraform/module/chatapp/main.tf
+++ terraform/module/chatapp/main.tf
@@ -5,27 +5,27 @@ resource "kubernetes_namespace_v1" "chat-app" {
   
 }
 
-resource "kubectl_manifest" "chatapp" {
-  depends_on = [ kubernetes_namespace_v1.chat-app ]
-  wait = true
-    yaml_body = templatefile("${path.module}/chatapp.yaml", {})
-}
-
-# resource "kubectl_manifest" "chatapp_database" {
+# resource "kubectl_manifest" "chatapp" {
 #   depends_on = [ kubernetes_namespace_v1.chat-app ]
 #   wait = true
-#     yaml_body = templatefile("${path.module}/chatapp-database.yaml", {})
+#     yaml_body = templatefile("${path.module}/chatapp.yaml", {})
 # }
 
-# resource "kubectl_manifest" "chatapp_backend" {
-#   depends_on = [ kubectl_manifest.chatapp_database ]
-#   wait = true
-#     yaml_body = templatefile("${path.module}/chatapp-backend.yaml", {})
-# }
+resource "kubectl_manifest" "chatapp_database" {
+  depends_on = [ kubernetes_namespace_v1.chat-app ]
+  wait = true
+    yaml_body = templatefile("${path.module}/chatapp-database.yaml", {})
+}
 
-# resource "kubectl_manifest" "chatapp_frontend" {
-#   depends_on = [ kubectl_manifest.chatapp_backend ]
-#   wait = true
-#     yaml_body = templatefile("${path.module}/chatapp-frontend.yaml", {})
-# }
+resource "kubectl_manifest" "chatapp_backend" {
+  depends_on = [ kubectl_manifest.chatapp_database ]
+  wait = true
+    yaml_body = templatefile("${path.module}/chatapp-backend.yaml", {})
+}
+
+resource "kubectl_manifest" "chatapp_frontend" {
+  depends_on = [ kubectl_manifest.chatapp_backend ]
+  wait = true
+    yaml_body = templatefile("${path.module}/chatapp-frontend.yaml", {})
+}
 
